9887e745ffc57b1c8f3b073a57388e17a403c9dc
Manish Twayana <manishtwayana@gmail.com>
2021-06-21 16:05:19 +1200

    Adding html and js to work

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.login-show').click(function(){
+        $('.loginpanel').show(2500);
+    });
+    $('.closing-login').click(function(){
+        $('.loginpanel').hide(2500);
+    });
+});
